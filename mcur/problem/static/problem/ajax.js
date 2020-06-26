d1a2bcd2ec7d96a5ed4842d14c4dfab1ed3efc92
kalan4iki <kalan-4@ya.ru>
2020-06-26 13:19:14 +0300

    Update ajax.js

--- mcur/problem/static/problem/ajax.js
+++ mcur/problem/static/problem/ajax.js
@@ -1,17 +1,17 @@
 var domain = 'https://skiog.ru/'
 
 window.onload = function() {
-  var not = document.getElementById('notif')
-  var spannot = document.getElementById('spannot')
-  var not1 = document.getElementById('not1')
-  var not2 = document.getElementById('not2')
-  var zapros = new XMLHttpRequest();
+  var notif = document.getElementById('notif'),
+      spannot = document.getElementById('spannot'),
+      not1 = document.getElementById('not1'),
+      not2 = document.getElementById('not2'),
+      zapros = new XMLHttpRequest();
   zapros.onreadystatechange = function() {
     if (zapros.readyState == 4){
       if (zapros.status == 200){
-        var data = JSON.parse(zapros.responseText)
-        notif.innerHTML = data['kolvosogl'] + data['kollno']
-        spannot.innerHTML = data['kolvosogl'] + data['kollno'] + ' Уведомлений'
+        var data = JSON.parse(zapros.responseText);
+        notif.innerHTML = data['kolvosogl'] + data['kollno'];
+        spannot.innerHTML = data['kolvosogl'] + data['kollno'] + ' Уведомлений';
         not1.innerHTML = '<i class="fas fa-envelope mr-2"></i>' + data['kollno'] + ' нераспределенных жалоб';
         not2.innerHTML = '<i class="fas fa-envelope mr-2"></i>' + data['kolvosogl'] + ' ответов на согласовании';
       }
